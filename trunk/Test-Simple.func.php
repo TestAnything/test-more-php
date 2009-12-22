a489ac9e99073cd11a85a3b9635cab41d69d3ca7
beyondlogical@gmail.com <beyondlogical@gmail.com@49bfcb56-ed2a-11de-bdfd-b3e7e4bb8034>
2009-12-22 21:43:12 +0000

    Conversion to proper class with seperate function exporting wrappers for procedural use. A bit of fiddling and the todo functions. Adding testsuite.

---
+++ trunk/Test-Simple.func.php
@@ -0,0 +1,13 @@
+<?php
+// Procedural wrapper for Test-More.php
+
+require_once('Test-Simple.php');
+
+global $__Test;
+$__Test = new TestSimple();
+
+// Expose public API for Simple methods as functions
+function plan()         { global $__Test; $args = func_get_args(); call_user_func_array(array($__Test,'plan'),$args); }
+function ok()           { global $__Test; $args = func_get_args(); call_user_func_array(array($__Test,'ok'),$args); }
+
+?>
