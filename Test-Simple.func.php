e530f1a89e2e939787b66460a68a8a6b9cfb63b9
beyondlogical <beyondlogical@49bfcb56-ed2a-11de-bdfd-b3e7e4bb8034>
2009-12-23 22:19:18 +0000

    Lots of small fixes, more tests.

--- Test-Simple.func.php
+++ Test-Simple.func.php
@@ -7,7 +7,8 @@ global $__Test;
 $__Test = new TestSimple();
 
 // Expose public API for Simple methods as functions
-function plan()         { global $__Test; $args = func_get_args(); call_user_func_array(array($__Test,'plan'),$args); }
-function ok()           { global $__Test; $args = func_get_args(); call_user_func_array(array($__Test,'ok'),$args); }
+function plan()         { global $__Test; $args = func_get_args(); return call_user_func_array(array($__Test,'plan'),$args); }
+function ok()           { global $__Test; $args = func_get_args(); return call_user_func_array(array($__Test,'ok'),$args); }
+function diag()         { global $__Test; $args = func_get_args(); return call_user_func_array(array($__Test,'diag'),$args); }
 
 ?>
